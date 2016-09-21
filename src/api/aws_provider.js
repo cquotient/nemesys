a7babf83989263eaa4d1010591a8b84dee08fcc0
Mike Torra <mike.torra@gmail.com>
2016-09-21 11:39:27 -0400

    add standardized asg helper

--- src/api/aws_provider.js
+++ src/api/aws_provider.js
@@ -10,4 +10,12 @@ function _get_ec2(region) {
 	}));
 }
 
+function _get_asg(region) {
+	return BB.promisifyAll(new AWS.AutoScaling({
+		region: region,
+		apiVersion: '2011-01-01'
+	}));
+}
+
 exports.get_ec2 = _get_ec2;
+exports.get_asg = _get_asg;
